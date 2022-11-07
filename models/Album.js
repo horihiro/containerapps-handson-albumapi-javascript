2a233d6f4e41abda6bd8357fcbd24d8197ce1a20
Hirofumi Horikawa <hori.kawa.shima.tool+github@gmail.com>
2022-11-07 15:28:41 +0900

    update model

--- models/Album.js
+++ models/Album.js
@@ -12,34 +12,6 @@ const albums = [
     artist: "The Blue-Green Stripes",
     price: 17.99,
     image_url: "https://aka.ms/albums-containerappslogo",
-  },
-  {
-    id: 3,
-    title: "Scale It Up",
-    artist: "KEDA Club",
-    price: 39.99,
-    image_url: "https://aka.ms/albums-kedalogo",
-  },
-  {
-    id: 4,
-    title: "Lost in Translation",
-    artist: "MegaDNS",
-    price: 39.99,
-    image_url: "https://aka.ms/albums-envoylogo",
-  },
-  {
-    id: 5,
-    title: "Lock Down your Love",
-    artist: "V is for VNET",
-    price: 39.99,
-    image_url: "https://aka.ms/albums-vnetlogo",
-  },
-  {
-    id: 6,
-    title: "Sweet Container O' Mine",
-    artist: "Guns N Probeses",
-    price: 39.99,
-    image_url: "https://aka.ms/albums-containerappslogo",
   }
 ];
 
